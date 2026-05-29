f8a2539198ef668240ac84708232bcb2fac912f6
Lucian Betke <lucian.betke@gmx.de>
2026-05-29 13:34:38 +0200

    refactor: Hardware-Header bereinigen

--- 2_Hardware/src/Hardware.h
+++ 2_Hardware/src/Hardware.h
@@ -1,16 +1,30 @@
+// ============================================================
+// File: Hardware.h
+// Zweck:
+//  - Zentrale Hardware-Schnittstelle fuer vorne und hinten
+//  - Globale Motor- und Encoder-Objekte bereitstellen
+//  - Hardware-Pins initialisieren
+// ============================================================
+
 #ifndef HARDWARE_H
 #define HARDWARE_H
+
 #include <Arduino.h>
+
 #include "src/RobotConfig.h"
 #include "src/HardwarePins.h"
 
 class Enc;
 class Motor;
-extern Enc   enc[WHEEL_COUNT];
+
+extern Enc enc[WHEEL_COUNT];
 extern Motor motor[WHEEL_COUNT];
 
 void hardware_begin(const HardwarePinSet& pins, bool resetEnc = true);
+
 void hardware_enableMotors();
 void hardware_disableMotors();
+
 void hardware_requestVist();
+
 #endif // HARDWARE_H
--- 2_Hardware/src/Motor.cpp
+++ 2_Hardware/src/Motor.cpp
@@ -1,15 +1,18 @@
-// Motor.cpp
+// ============================================================
+// File: Motor.cpp
+// Zweck:
+//  - Steuerung eines einzelnen Motors im 2-Pin-Modus
+//  - Richtung, PWM-Leistung, Bremse
+//  - Optionale Encoder-Anbindung
+// ============================================================
+
 #include "Motor.h"
 
-// ===== Motor =====
-Motor::Motor() : _pin1(0), _pin2(0), _enc(nullptr)
+Motor::Motor()
+    : _pin1(0),
+    _pin2(0),
+    _enc(nullptr)
 {
-}
-
-void Motor::init() {
-    pinMode(_pin1, OUTPUT);
-    pinMode(_pin2, OUTPUT);
-    bremse(false); // Coast
 }
 
 void Motor::begin(uint8_t pin1, uint8_t pin2, Enc& enc)
@@ -19,13 +22,46 @@ void Motor::begin(uint8_t pin1, uint8_t pin2, Enc& enc)
     _enc = &enc;
 }
 
-void Motor::vor(uint8_t pwm) { analogWrite(_pin1, pwm); digitalWrite(_pin2, LOW); }
-void Motor::rueck(uint8_t pwm) { analogWrite(_pin2, pwm); digitalWrite(_pin1, LOW); }
+void Motor::init()
+{
+    pinMode(_pin1, OUTPUT);
+    pinMode(_pin2, OUTPUT);
 
-void Motor::bremse(bool art) {
-    if (art) { digitalWrite(_pin1, HIGH); digitalWrite(_pin2, HIGH); }   // aktiv bremsen
-    else { digitalWrite(_pin1, LOW);  digitalWrite(_pin2, LOW); }   // auslaufen
+    bremse(false);
 }
 
-void Motor::attachEnc(Enc& enc) { _enc = &enc; }
-Enc* Motor::enc() const { return _enc; }
+void Motor::vor(uint8_t pwm)
+{
+    analogWrite(_pin1, pwm);
+    digitalWrite(_pin2, LOW);
+}
+
+void Motor::rueck(uint8_t pwm)
+{
+    analogWrite(_pin2, pwm);
+    digitalWrite(_pin1, LOW);
+}
+
+void Motor::bremse(bool art)
+{
+    if (art)
+    {
+        digitalWrite(_pin1, HIGH);
+        digitalWrite(_pin2, HIGH);
+    }
+    else
+    {
+        digitalWrite(_pin1, LOW);
+        digitalWrite(_pin2, LOW);
+    }
+}
+
+void Motor::attachEnc(Enc& enc)
+{
+    _enc = &enc;
+}
+
+Enc* Motor::enc() const
+{
+    return _enc;
+}
